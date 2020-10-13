2d324fc9341ae0670d98d2b938c859e34bf99508
ahilanc <ahilan01.c@gmail.com>
2020-10-13 10:14:13 +0530

    Modified few scripts in Test Runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,15 +1,21 @@
 package runner;
 
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {
-		"src/test/resources/Features" }, glue = "com.stepDef", monochrome = true, dryRun = false, strict = true, tags = {
-				"@test" }, plugin = { "pretty", "json:Reports\\output.json" })
-
+@CucumberOptions(
+		features = "src/test/resources/Features",
+		glue = {"com.stepDef"},
+		tags = {"@test"},
+		plugin = {"pretty", "json:target/cucumber.json"}
+)
 public class TestRunner {
-
+	@AfterClass
+	public static void tearDown() {
+		HTMLReporter.main(new String[]{});
+	}
 }
